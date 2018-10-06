b9f5cd58dd7e9ea41acbea3c07d9104709fcd747
Pau <Pau@localhost.localdomain>
2018-10-06 13:38:11 +0200

    Versió 4, falten corregir els minuts

--- Programacio/python/Superior/Estructures_Seqüencials_Ex4.py
+++ Programacio/python/Superior/Estructures_Seqüencials_Ex4.py
@@ -4,7 +4,7 @@
 # Pau Martín Arnau
 # isx46420653
 # 05/10/18
-# Versió acabada
+# Falten corregir els minuts, poden passar de 60
 
 # Que demani una hora amb valors enters, transformar l'hora donada a un format correcte,
 # en cas de que no ho sigui, i mostrar per pantalla l'hora correcte que ha introduit 
@@ -13,20 +13,42 @@
 # S'ha de llegir una hora amb valors enters i amb el format: hh mmm sss
 # Mostrara per sortida el format correcte de l'hora en cas de que estigui malament. 
 
+# Variables on entra l'usuari l'hora.
 segons = int(input("Quants segons vols? "))
 
 minuts = int(input("Quants minuts vols? "))
 
 hores = int(input("Quantes hores vols? "))
 
-if (segons < 60):
+# Calcularem l'excès de segons, es a dir, que passin de 60 per a després 
+# poder augmentar-los als minuts
 
-	segons = segons + (segons % 60)
+augment_segons = (segons // 60)
 
-minuts = segons = segons + (segons % 60)
+# Fem la operació per a saber quants segons en queden, en cas de tenir més de 60
+# s'hauran de restar la quantitat sobrant per a que quedi un numero inferior a 60 i 
+# despres afegir aquest numero als minuts totals. 
 
-hores = hores + (hores % 60)
+segons_totals = segons - augment_segons*60
 
+# Calcularem l'excès de minuts, es a dir, que passin de 60 per a després 
+# poder augmentar-los a les hores
+
+augment_minuts = (minuts // 60)
+
+# Fem la operació per a saber quants minuts en queden, en cas de tenir més de 60
+# s'hauran de restar la quantitat sobrant per a que quedi un numero inferior a 60 i 
+# despres afegir aquest numero a les hores totals, a més també afegim els segons que 
+# em calculat anteriorment que superaven els 60.  
+
+minuts_totals = minuts - augment_minuts*60 + augment_segons
+
+# Les hores no les hem de calcular, ja que podem posar més de 24, el que si que hem de fer
+# es afegir-les el numero d'augment dels minuts.
+
+hores_totals = hores + augment_minuts
+
+# Fem un print de les variables. 
 print ("")
-print ("El total de temps introduit és: ",hores, "hores, ", minuts, "minuts i ", segons, "segons")
+print ("El total de temps introduit és:",hores_totals, "hores,", minuts_totals, "minuts i", segons_totals, "segons")
 print ("")
